2701567fa5c549efa0d1f589c08a37777b60f582
Bar Segev <barse@wix.com>
2023-09-05 14:33:59 +0300

    started app

---
+++ tic-tac-toe/src/App.js
@@ -0,0 +1,53 @@
+import { useState } from "react";
+import "./App.css";
+
+const Cell = ({ currentPlayer, toggleCurrentPlayer }) => {
+  const [val, setVal] = useState(null);
+  const onClickHandler = () => {
+    if (val === null) {
+      setVal(currentPlayer);
+      toggleCurrentPlayer();
+    }
+  };
+
+  return <td onClick={onClickHandler}>{val}</td>;
+};
+
+const Row = ({ currentPlayer, toggleCurrentPlayer }) => (
+  <tr>
+    {[1, 2, 3].map((i) => (
+      <Cell
+        key={i}
+        currentPlayer={currentPlayer}
+        toggleCurrentPlayer={toggleCurrentPlayer}
+      />
+    ))}
+  </tr>
+);
+
+function App() {
+  const [currentPlayer, setCurrentPlayer] = useState("X");
+  const toggleCurrentPlayer = () =>
+    currentPlayer === "X" ? setCurrentPlayer("O") : setCurrentPlayer("X");
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Tic tac toe</h1>
+        <h2>Current player is: {currentPlayer}</h2>
+        <table>
+          <tbody>
+            {[1, 2, 3].map((i) => (
+              <Row
+                key={i}
+                currentPlayer={currentPlayer}
+                toggleCurrentPlayer={toggleCurrentPlayer}
+              />
+            ))}
+          </tbody>
+        </table>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ tic-tac-toe/src/App.test.js
@@ -0,0 +1,37 @@
+import { render, screen } from "@testing-library/react";
+import App from "./App";
+
+test("renders Tic tac toe heading", () => {
+  render(<App />);
+  const [headingElement] = screen.getAllByRole("heading");
+  expect(headingElement).toHaveTextContent(/tic tac toe/i);
+});
+
+test("should render current player", () => {
+  render(<App />);
+  const headingElement = screen.getByText(/current player/i);
+  expect(headingElement).toBeInTheDocument();
+});
+
+test("renders Tic Tac Toe Table", () => {
+  render(<App />);
+  const tableElement = screen.getByRole("table");
+  expect(tableElement).toBeInTheDocument();
+  const rows = screen.getAllByRole("row");
+  expect(rows).toHaveLength(3);
+  const cells = screen.getAllByRole("cell");
+  expect(cells).toHaveLength(9);
+});
+
+test("cell click - should render current player", () => {
+  const [cell1, cell2] = screen.getAllByRole("cell");
+  expect(cell1).toBeEmptyDOMElement();
+  cell1.click();
+  expect(cell1).toHaveTextContent("X");
+  cell1.click();
+  expect(cell1).toHaveTextContent("X"); // should stay X
+
+  expect(cell2).toBeEmptyDOMElement();
+  cell2.click();
+  expect(cell2).toHaveTextContent("O");
+});
